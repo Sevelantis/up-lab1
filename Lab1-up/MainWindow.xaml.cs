dced3a039bd0d3b4026e3978fb80a2cc36062ca1
Miron <hamer223@o2.pl>
2020-10-15 15:39:43 +0200

    added resolution change option

--- Lab1-up/MainWindow.xaml.cs
+++ Lab1-up/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.ComTypes;
 using System.Windows;
 using System.Windows.Controls;
 using Camera_NET;
@@ -12,33 +13,60 @@ namespace Lab1_up
     public partial class MainWindow : Window
     {
         private CameraChoice _CameraChoice;
+        private ResolutionList resolutions;
+        private IMoniker camera_moniker;
+        private int res_index = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //on loaded:
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+            //init
             _CameraChoice = new CameraChoice();
 
             _CameraChoice.UpdateDeviceList();
 
             //update combobox
-            cmbBox.ItemsSource = _CameraChoice.Devices;
+            cameraCmbBox.ItemsSource = _CameraChoice.Devices;
         }
 
+        // on window closed:
         private void Window_Closed(object sender, EventArgs e)
         {
             cameraControl.CameraControl.CloseCamera();
         }
-
-        private void CmbBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        
+        //on combo box camera selection:
+        private void CmbBoxCameraChanged(object sender, SelectionChangedEventArgs e)
         {
             if (_CameraChoice.Devices.Count > 0)
             {
-                var camera_moniker = _CameraChoice.Devices[cmbBox.SelectedIndex].Mon;
-                cameraControl.CameraControl.SetCamera(camera_moniker, null);
+                camera_moniker = _CameraChoice.Devices[cameraCmbBox.SelectedIndex].Mon;
+                resolutions = Camera.GetResolutionList(camera_moniker);
+
+                //update resolution combo box
+                resolutionCmbBox.ItemsSource = resolutions;
+
+                cameraControl.CameraControl.SetCamera(camera_moniker, resolutions[res_index]);
+
             }
         }
+
+        //on combo box resolution selection:
+        private void CmbBoxResolutionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_CameraChoice.Devices.Count > 0)
+            {
+                resolutions = Camera.GetResolutionList(camera_moniker);
+                res_index = resolutionCmbBox.SelectedIndex;
+                cameraControl.CameraControl.SetCamera(camera_moniker, resolutions[res_index]);
+            }
+        }
+
+
     }
 }
